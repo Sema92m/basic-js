fe0556c2ef69c9a3977d72ef07ab8d431cb387c8
sema92m <sema92m@wp.pl>
2023-04-11 21:11:01 -0700

    feat: add cats-count task

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit() {
+ 
 }
+deleteDigit();
 
 module.exports = {
-  deleteDigit
+    deleteDigit,
 };
+
+
+
+// function a(n) {
+//     const minNum = (+String(n).split("").sort((a, b) => a - b)[0]);
+//     // console.log(minNum);
+//     let result = [];
+//     let c =    [] ;
+//      +String(n).split("").reverse().forEach((element) => {
+//       console.log(element);
+//       console.log(typeof +element);
+//               if (+element !== minNum || (+element === minNum && c.length === 1)) {
+//                 result.push(element);
+//                 console.log(1);
+//                 }
+//                 if (+element === minNum && c.length < 1) {
+//                     c.push(element);
+//                     console.log(2);
+//                 }
+                
+                
+//             });
+
+//     return console.log([minNum, +result.reverse().join("")]);
+// }
+// a(342);
+//342 dont work
+
+
+
+// function deleteDigit(n) {
+//  let arr = String(n).split("");
+//  let res = [];
+ 
+ 
+//  return console.log(res);
+// }
+// deleteDigit(222219);
