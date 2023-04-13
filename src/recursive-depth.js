b10dd0ee474e7a5ad605ff361333f8edbec7425e
sema92m <sema92m@wp.pl>
2023-04-12 21:02:47 -0700

    feat: add what-sason task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,11 +1,11 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,12 +13,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    calculateDepth(arr) {
+        let d = 1;
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) {
+                const nest = calculateDepth(arr[i]) + 1;
+                d = Math.max(d, nest);
+            }
+        }
+        return d;
+    }
 }
 
 module.exports = {
-  DepthCalculator
+    DepthCalculator,
 };
+
+//npm run test ./test/recursive-depth.teest.js
+
+// let arr = [1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]];
+// function calculateDepth1(arr) {
+//     let d = 1;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (Array.isArray(arr[i])) {
+//             const nest = calculateDepth1(arr[i]) + 1;
+//             d = Math.max(d, nest);
+//         }
+//     }
+
+//     return d;
+// }
+// console.log(calculateDepth1(arr));
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,75 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+    if (!date) {
+        return "Unable to determine the time of year!";
+    }
+    if (!Date.parse(date)) {
+        throw new Error("Invalid date!");
+    }
+    try {
+        date.getUTCSeconds();
+    } catch {
+        throw new Error("Invalid date!");
+    }
+
+    let month = date.getMonth() + 1;
+
+    if (month < 3 || month === 12) {
+        return "winter";
+    } else if (month >= 3 && month < 6) {
+        return "spring";
+    } else if (month >= 6 && month < 9) {
+        return "summer";
+    } else if (month > 8 && month < 12) {
+        return "fall";
+    }
 }
+getSeason();
 
 module.exports = {
-  getSeason
+    getSeason,
 };
+
+
+
+
+// npm run test ./test/what-season.test.js
+
+// function getSeason(date) {
+//   if (!date) {
+//       return "Unable to determine the time of year!";
+//   }
+//   if (!Date.parse(date)) {
+//       throw new Error("Invalid date!");
+//   }
+//   try {
+//       date.getUTCSeconds();
+//   } catch {
+//       throw new Error("Invalid date!");
+//   }
+
+//   let month = date.getMonth() + 1;
+
+//   if (month < 3 || month === 12) {
+//       return "winter";
+//   } else if (month >= 3 && month < 6) {
+//       return "spring";
+//   } else if (month >= 6 && month < 9) {
+//       return "summer";
+//   } else if (month > 8 && month < 12) {
+//       return "fall";
+//   }
+// }
+// getSeason();
